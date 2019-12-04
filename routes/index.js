424c592de2c9d8c0f88098aace445fddadd52dd5
Khalel <KAbaquin@west-mec.org>
2019-12-04 15:24:17 -0700

    Update 2
    figuring out how to implement the pokeapi

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Final Project' });
 });
 
+// router.get('/', function(req, res, next) {
+// res.render('index', { poke: sylveon });
+// });
+
+// function sylveon() {
+//   fetch('https://pokeapi.co/api/v2/pokemon/sylveon/')
+//   .then( res  => res.json() )
+//   .then(
+//       data => {
+//           const{...img} = data.sprites;
+//           console.log( img.front_default );
+//           selector = document.querySelector('.pokemon');
+//           pokeIMG = `<img src="${img.front_default}">`;
+//           selector.innerHTML = pokeIMG;
+//       }
+//   );
+// }
+
 module.exports = router;
